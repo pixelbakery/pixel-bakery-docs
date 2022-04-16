9b6997472501b431b465eff0b00d6afbd5abc1ca
Jordan Lambrecht <jordan@pixelbakery.co>
2022-04-16 18:05:19 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './styles/globals.css',
+    './theme.config.js',
+    './pages/**/*.md',
+    './pages/**/*.mdx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
